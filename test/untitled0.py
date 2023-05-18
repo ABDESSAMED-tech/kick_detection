b053801d1c1a16767eb01e7f2d464f8797d8031b
TAZIReda/ <tazir-reda@outlook.fr>
2023-05-18 09:50:13 +0100

    generation dataset

--- test/untitled0.py
+++ test/untitled0.py
@@ -12,24 +12,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def feature_rep_mean(window,thrsh_gasa,thrdh_mfop,thrsh_spp,thrsh_tva):
-    gasa, mfoa, spp, mfop, tva = False, False, False, False, False
+    gasa, mfoa, spp, mfop, tva = 0, 0, 0, 0, 0
     # threshold for each feature std+mean of the feature when the kick is happen
     if window['GASA (mol/mol)'].mean() >=thrsh_gasa:
-        gasa = True
+        gasa = 1
         
     elif window['TVA (m3)'].mean() >=thrsh_tva :
-        tva = True
+        tva = 1
         
     elif window['SPPA (kPa)'].mean() <=thrsh_spp:
-        spp = True
+        spp = 1
         
     # elif window['variation_MFOA (m3/s)'].sum() <=0:
     #     mfoa = True
        
     elif  window['MFOP ((m3/s)/(m3/s))'].mean() <=thrdh_mfop:
-        mfop = True
-
+        mfop = 1
+    #print (gasa, mfop, spp, tva)
     return [gasa, mfop, spp, tva]
+
 def kick_detection(featurs):
     
     if featurs[0] and  (featurs[1]and  featurs[2]and  featurs[3]):
@@ -70,7 +71,7 @@ wind_var={
     
     }
     
-df=pd.read_excel(r'C:\Users\hp\Desktop\M2\PFE\Code\code pfe\Coud source\Code\dataset\Well-6.xlsx')
+df=pd.read_excel(r'C:\Users\DropZone\Desktop\Nouveau\kick_detection\dataset\Well-6.xlsx')
 df['kick_recognition'] = 111 #just initialzation
 
 cols_variation=['variation_TVA (m3)', 'variation_SPPA (kPa)',
@@ -85,25 +86,32 @@ spp=[-1896.058256,-6.894757]
 mfop=[-0.060000,0]
 gasa=[ -0.006400,-0.000100]
 
-thrsh_gasa,thrdh_mfop,thrsh_spp,thrsh_tva= 0.056960,0.110917,4987.867887,73.482569
+#thrsh_gasa,thrdh_mfop,thrsh_spp,thrsh_tva= 0.056960,0.110917,4987.867887,73.482569
+thrsh_gasa,thrdh_mfop,thrsh_spp,thrsh_tva= 0,0,0,0
+
+train_data=[]
 for i in range(len(df)-window_size+1):
         window = df.iloc[i:i+window_size]
-        window_varation(window,wind_var)
+       # window_varation(window,wind_var)       #### i de status a au min avoir un 1 dans un window
+        train=feature_rep_mean(window,thrsh_gasa,thrdh_mfop,thrsh_spp,thrsh_tva)
+        train.append(df['STATUS'][i])
+        train_data.append(train)
+        #print(feature_rep_mean(window,thrsh_gasa,thrdh_mfop,thrsh_spp,thrsh_tva))
         df['kick_recognition'][i:i+window_size]=kick_detection(feature_rep_mean(window,thrsh_gasa,thrdh_mfop,thrsh_spp,thrsh_tva))
-        
+print(train_data)     
         
 ACC=sum((df['STATUS']==df['kick_recognition']))/df.shape[0] #calculate accuracy of algorithme
-print("Accuracy",ACC)
+#print("Accuracy",ACC)
 
 data=df[df['STATUS']==1]#for get just where status=1
 statu_acc=sum((data['STATUS']==data['kick_recognition']))/data.shape[0]
-print('status accuracy',statu_acc)
+#print('status accuracy',statu_acc)
 df[['STATUS','kick_recognition']].plot()
 
 
 
-for i in data:
-    print(i,data[i].describe())
+#for i in data:
+   # print(i,data[i].describe())
 
 # data.plot()
 # # data.describe()
